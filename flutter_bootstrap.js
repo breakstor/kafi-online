1b2d3224033792f75a6856545f55aa211663adc5
Break Technology <155421526+breakstor@users.noreply.github.com>
2026-08-30 16:29:37 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3228758967" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2310160901" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
